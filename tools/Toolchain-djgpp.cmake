7587b660f037d8236c322dcc7ea2e18e593a931b
jlrn7 <jlrn7@users.noreply.github.com>
2019-08-08 18:19:28 -0500

    Update Toolchain-djgpp.cmake
    fixed flags for msdos-djgpp compilation

--- tools/Toolchain-djgpp.cmake
+++ tools/Toolchain-djgpp.cmake
@@ -2,11 +2,9 @@
 
 set (IS_CROSSCOMPILING "YES")
 set (IS_32_BIT "32-")
-
-set (CMAKE_SYSTEM_NAME DJGPP)
+set (CMAKE_C_FLAGS "-DDJGPP")
 
 set (CMAKE_C_COMPILER i586-pc-msdosdjgpp-gcc)
 set (CMAKE_CXX_COMPILER i586-pc-msdosdjgpp-g++)
-set (CMAKE_RC_COMPILER i586-pc-msdosdjgpp-windres)
 
 set (CMAKE_FIND_ROOT_PATH /usr/bin)
